4dbfea4bcbf781e479fcb8e9d6fa1b3023c9f448
Brage Hole Nestande <braghnes@gmail.com>
2022-01-11 12:43:06 +0100

    privat helsing

--- commands/helsing.js
+++ commands/helsing.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('user')
+		.setName('helsing')
 		.setDescription('Replies with Hei guth!'),
 	async execute(interaction) {
-		await interaction.reply(`Hei! Kordan går det ${interaction.user.tag}?`);
+		await interaction.reply({ content: `Hei! Kordan går det ${interaction.user.tag}?`, ephemeral: true});
 	},
 };
