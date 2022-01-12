fd220f27f1929f48498d2d4a7b8b5abe074b5a32
Brage Hole Nestande <braghnes@gmail.com>
2022-01-12 21:43:33 +0100

    k

--- inProduction/calculate.js
+++ inProduction/calculate.js
@@ -8,8 +8,8 @@ module.exports = {
 			option.setName('')
 				.setDescription('')),
 	async execute(interaction) {
-		const user = interaction.options.getUser('target');
-		return interaction.reply({ content: `You wanted to kick: ${user.username}`, ephemeral: true });
+		const user = interaction.options.getUser('');
+		return interaction.reply({ content: `{} {} {} = `, ephemeral: true });
 	},
 };
 
