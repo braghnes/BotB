fcaf391725aa3baa8ba094ec2b9f270fa9738e71
Brage Hole Nestande <braghnes@gmail.com>
2022-01-20 09:35:37 +0100

    kommentert koda

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,9 +3,16 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
+/*
+ * Finner og registrer kommandoene du har laget i discord
+ */
+
+// Et tomt array som kjem til å inneholde kommandoer
 const commands = [];
+// Henter alle filene som ender med .js fra commands-mappa til commandFiles
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+//Går gjennom alle filene fra commandFiles og legger dem til arrayet commands
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
@@ -13,9 +20,21 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+//Skjekker om alt er oppe og går, sender error om noko gale skjer
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded all application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
 
 
 
--- index.js
+++ index.js
@@ -1,42 +1,32 @@
+//
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
-const { config } = require("dotenv")
 
+//ny client
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+//Henter inn eventer fra events-mappa og aktiverer dem i clienten over slik
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-//loading picture?
-config({
-    path: __dirname + "/.env"
-});
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+//Hent inn kommandoer og lagre dem i client
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
-	console.log('Ready!');
-	client.user.setPresence({ activities: [{ name: 'Crab Game' }], status: 'online' });
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
-
+// Logger inn i Discord med clientens token
 client.login(token);
